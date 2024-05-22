91820543cea8677d1f6f3d5de50edd3cdc591077
Sungyon Park <mokhwa96@gmail.com>
2024-05-22 17:10:32 +0900

    auto

---
+++ Object_Oriented_Programming_Fundamentals/ConstructorExample.java
@@ -0,0 +1,21 @@
+package Object_Oriented_Programming_Fundamentals;
+
+public class ConstructorExample {
+    public static void main(String[] args){
+        Constructor constructor1 = new Constructor();
+        Constructor constructor2 = new Constructor("Hello World");
+        Constructor constructor3 = new Constructor(5, 10);
+
+    }
+}
+class Constructor {
+    Constructor(){
+        System.out.println("1번 생성자");
+    }
+    Constructor(String str){
+        System.out.println("2번 생성자");
+    }
+    Constructor(int a, int b){
+        System.out.println("3번 생성자");
+    }
+}
---
+++ Object_Oriented_Programming_Fundamentals/ConstructorExample2.java
@@ -0,0 +1,28 @@
+package Object_Oriented_Programming_Fundamentals;
+
+public class ConstructorExample2 {
+    public static void main(String[] args){
+        Car2 c = new Car2("X", "빨간색", 250);
+        System.out.println("제 차는 " + c.getModelName() + "이고, 컬러는 " + c.getColor() + "입니다.");
+    }
+}
+
+class Car2 {
+    private String modelName;
+    private String color;
+    private int maxSpeed;
+
+    public Car2(String modelName, String color, int maxSpeed){
+        this.modelName = modelName;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public String getModelName(){
+        return modelName;
+    }
+    public String getColor(){
+        return color;
+    }
+}
+
